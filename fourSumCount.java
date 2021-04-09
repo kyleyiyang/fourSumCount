6a8617836bbf44afebb9f3e4be0c516d94bb0db0
kyleyiyang <kyleyiyang@gmail.com>
2021-04-08 21:10:36 -0400

    Create fourSumCount.java

---
+++ fourSumCount.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer,Integer> map =new HashMap<>();
+        int count=0;
+        for (int a:A) {
+            for (int b:B) {
+                map.put(a+b,map.getOrDefault(a+b,0)+1);
+            }
+        }
+        for (int c:C) {
+            for (int d:D) {
+                count+=map.getOrDefault(-c-d,0);
+            }
+        }
+        return count;
+    }
+}
